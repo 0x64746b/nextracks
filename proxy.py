3d189a72e62cfe4602c49e2ab6225fe5098796c0
dtk <dtk@gmx.de>
2025-07-31 16:53:23 +0200

    refactor: simplify serving of `index.html`

--- proxy.py
+++ proxy.py
@@ -1,14 +1,12 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse, Response
+from fastapi.responses import FileResponse, Response
 import requests
 
 app = FastAPI()
 
 @app.get("/index.html")
-def serve() -> HTMLResponse:
-    with open("index.html") as f:
-        html = f.read()
-        return HTMLResponse(content=html, status_code=200)
+def serve() -> FileResponse:
+    return FileResponse("index.html")
 
 @app.get("/")
 def fetch_track(track: str) -> Response:
