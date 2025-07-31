ab914968c268800510cbdde9937636f871739fb8
dtk <dtk@gmx.de>
2025-07-31 17:12:15 +0200

    feat: read Nextcloud domain from env var

--- proxy.py
+++ proxy.py
@@ -1,3 +1,5 @@
+import os
+
 from fastapi import FastAPI
 from fastapi.responses import FileResponse, Response
 import requests
@@ -10,5 +12,5 @@ def serve() -> FileResponse:
 
 @app.get("/track/{id}")
 def fetch_track(id: str) -> Response:
-    xml = requests.get(f"https://cloud.execvebin.sh/index.php/s/{id}/download").content
+    xml = requests.get(f"https://{os.environ["NEXTRACKS_NC_DOMAIN"]}/index.php/s/{id}/download").content
     return Response(content=xml, media_type="application/xml")
