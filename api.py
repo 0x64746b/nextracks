51cce99353207d6fc34e65eed25b1f19808e8375
dtk <dtk@gmx.de>
2025-07-31 23:53:00 +0200

    feat: deliver preview image via API

--- api.py
+++ api.py
@@ -10,6 +10,10 @@ app = FastAPI()
 def serve() -> FileResponse:
     return FileResponse("index.html")
 
+@app.get("/preview-image")
+def generate_preview_image() -> FileResponse:
+    return FileResponse("public/2024-09-08.jpg")
+
 @app.get("/track/{id}")
 def fetch_track(id: str) -> Response:
     xml = requests.get(f"https://{os.environ['NEXTRACKS_NC_DOMAIN']}/index.php/s/{id}/download").content
