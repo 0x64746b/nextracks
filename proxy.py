c975081c6bb96d6d2ff79e32450d52427809feb8
dtk <dtk@gmx.de>
2025-07-31 17:01:14 +0200

    feat: make the track ID a path param in the proxy

--- proxy.py
+++ proxy.py
@@ -4,11 +4,11 @@ import requests
 
 app = FastAPI()
 
-@app.get("/index.html")
+@app.get("/")
 def serve() -> FileResponse:
     return FileResponse("index.html")
 
-@app.get("/")
-def fetch_track(track: str) -> Response:
-    xml = requests.get(f"https://cloud.execvebin.sh/index.php/s/{track}/download").content
+@app.get("/track/{id}")
+def fetch_track(id: str) -> Response:
+    xml = requests.get(f"https://cloud.execvebin.sh/index.php/s/{id}/download").content
     return Response(content=xml, media_type="application/xml")
