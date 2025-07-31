4cd0a1e80d917f0f18190ed8871a859e9fc5655f
dtk <dtk@gmx.de>
2025-07-31 15:18:41 +0200

    fix: return GPX data as XML
    Previous it was declared as JSON.

--- proxy.py
+++ proxy.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
-from fastapi.responses import JSONResponse
+from fastapi.responses import Response
 import requests
 
 app = FastAPI()
 
 @app.get("/")
-def fetch_track(track: str) -> JSONResponse:
-    return requests.get(f"https://cloud.execvebin.sh/index.php/s/{track}/download").content
+def fetch_track(track: str) -> Response:
+    xml = requests.get(f"https://cloud.execvebin.sh/index.php/s/{track}/download").content
+    return Response(content=xml, media_type="application/xml")
