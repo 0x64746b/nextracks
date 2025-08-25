2983223dbd1be931a32eff51b1c40ad53e823257
dtk <dtk@gmx.de>
2025-08-25 18:12:13 +0200

    feat: generate dynamic preview image

--- api.py
+++ api.py
@@ -1,9 +1,17 @@
+from io import BytesIO
 import os
+from tempfile import NamedTemporaryFile
 
 from fastapi import FastAPI, Request
 from fastapi.responses import FileResponse, HTMLResponse, Response
 from fastapi.templating import Jinja2Templates
+from gpx_converter import Converter
+import matplotlib
+from pandas import DataFrame
 import requests
+
+# Use non-interactive backend for plotting
+matplotlib.use('agg')
 
 app = FastAPI()
 
@@ -13,11 +21,30 @@ templates = Jinja2Templates(directory="templates")
 def serve(request: Request) -> HTMLResponse:
     return templates.TemplateResponse(request=request, name="index.html")
 
-@app.get("/preview-image")
-def generate_preview_image() -> FileResponse:
-    return FileResponse("public/2024-09-08.jpg")
+@app.get("/preview-image/{id}")
+def generate_preview_image(id: str) -> Response:
+    response = requests.get(f"https://{os.environ['NEXTRACKS_NC_DOMAIN']}/index.php/s/{id}/download")
+    gpx: DataFrame = _parse_gpx(response.content)
+    image: bytes = _plot_gpx(gpx)
+
+    return Response(content=image, media_type="image/png")
 
 @app.get("/track/{id}")
 def fetch_track(id: str) -> Response:
     xml = requests.get(f"https://{os.environ['NEXTRACKS_NC_DOMAIN']}/index.php/s/{id}/download").content
     return Response(content=xml, media_type="application/xml")
+
+def _parse_gpx(xml: bytes) -> DataFrame:
+    with NamedTemporaryFile(delete_on_close=False, suffix='.gpx') as tmp_file:
+         tmp_file.write(xml)
+         tmp_file.close()
+         return Converter(tmp_file.name).gpx_to_dataframe()
+
+def _plot_gpx(data: DataFrame) -> bytes:
+    data.plot(x='longitude', y='latitude')
+
+    image_file = BytesIO()
+    matplotlib.pyplot.savefig(image_file, format='png')
+    image_file.seek(0)
+
+    return image_file.read()
