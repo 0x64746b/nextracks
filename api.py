c24c0f365b954323af1154030e480dcbf40f63da
Daniel Kreischer <daniel.kreischer@deutschebahn.com>
2025-08-26 15:32:55 +0200

    feat: generate preview from all tracks

--- api.py
+++ api.py
@@ -1,13 +1,14 @@
 from io import BytesIO
 import os
 from tempfile import NamedTemporaryFile
+from typing import Annotated
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Query, Request
 from fastapi.responses import FileResponse, HTMLResponse, Response
 from fastapi.templating import Jinja2Templates
 from gpx_converter import Converter
 import matplotlib
-from pandas import DataFrame
+import pandas as pd
 import requests
 
 # Use non-interactive backend for plotting
@@ -22,11 +23,13 @@ def serve(request: Request) -> HTMLResponse:
     request.scope['scheme'] = 'https' # FIXME: force generation of `HTTPS` URLs
     return templates.TemplateResponse(request=request, name="index.html")
 
-@app.get("/preview-image/{id}")
-def generate_preview_image(id: str) -> Response:
-    response = requests.get(f"https://{os.environ['NEXTRACKS_NC_DOMAIN']}/index.php/s/{id}/download")
-    response.raise_for_status()
-    gpx: DataFrame = _parse_gpx(response.content)
+@app.get("/preview-image")
+def generate_preview_image(track: Annotated[list[str], Query()]) -> Response:
+    gpx = pd.DataFrame()
+    for id in track:
+        response = requests.get(f"https://{os.environ['NEXTRACKS_NC_DOMAIN']}/index.php/s/{id}/download")
+        response.raise_for_status()
+        gpx = pd.concat([gpx, _parse_gpx(response.content)])
     image: bytes = _plot_gpx(gpx)
 
     return Response(content=image, media_type="image/png")
@@ -37,13 +40,13 @@ def fetch_track(id: str) -> Response:
     response.raise_for_status()
     return Response(content=response.content, media_type="application/xml")
 
-def _parse_gpx(xml: bytes) -> DataFrame:
+def _parse_gpx(xml: bytes) -> pd.DataFrame:
     with NamedTemporaryFile(delete_on_close=False, suffix='.gpx') as tmp_file:
          tmp_file.write(xml)
          tmp_file.close()
          return Converter(tmp_file.name).gpx_to_dataframe()
 
-def _plot_gpx(data: DataFrame) -> bytes:
+def _plot_gpx(data: pd.DataFrame) -> bytes:
     data.plot(x='longitude', y='latitude')
 
     image_file = BytesIO()
